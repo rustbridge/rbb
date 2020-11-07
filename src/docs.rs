af0ffcd6917605692d33cb827b1ceab787900347
mokou <mokou@posteo.de>
2020-11-07 13:45:05 +0100

    feat(docs): Add sitemap generation

--- src/docs.rs
+++ src/docs.rs
@@ -3,15 +3,25 @@ use tide::{Response, Request, http::mime};
 use comrak::{markdown_to_html, ComrakOptions};
 use async_std::fs::read_to_string;
 use async_std::path::PathBuf;
+use walkdir::WalkDir;
 use std::convert::TryInto;
 
 #[derive(Template)]
 #[template(path = "docs.html")]
-struct DocsTemplate {
+struct DocsTemplate<'a> {
     content: String,
+    sitemap: &'a Sitemap,
+
+}
+
+#[derive(Debug, Clone)]
+pub struct Sitemap {
+    pub num_entries: usize,
+    pub entries: Vec<String>,
 }
 
 pub async fn docs_handler(req: Request<()>) -> Result<Response, tide::Error> {
+    let sitemap = req.ext::<Sitemap>().unwrap();
     let filename = req.param("path").unwrap_or("index");
     let path: PathBuf = ["docs", format!("{}.md", filename).as_ref()].iter().collect();
     if !path.exists().await {
@@ -21,10 +31,28 @@ pub async fn docs_handler(req: Request<()>) -> Result<Response, tide::Error> {
     let html = markdown_to_html(&md, &ComrakOptions::default());
     let template: tide::Body = DocsTemplate {
         content: html,
+        sitemap,
     }.try_into().unwrap();
     let res = Response::builder(200)
         .body(template)
         .content_type(mime::HTML)
         .build();
     Ok(res)
+}
+
+pub fn construct_sitemap() -> Result<Sitemap, std::io::Error> {
+    let mut res = Vec::new();
+    let iter = WalkDir::new("docs/")
+        .into_iter()
+        .filter(|e| !e.as_ref().ok().unwrap().file_type().is_dir());
+    for entry in iter {
+        let entry = entry?;
+        let mut path = entry.path().to_path_buf();
+        path.set_extension("html");
+        res.push(path.into_os_string().into_string().unwrap());
+    }
+    Ok(Sitemap {
+        num_entries: res.len(),
+        entries: res,
+    })
 }
--- src/main.rs
+++ src/main.rs
@@ -14,14 +14,30 @@ async fn index_handler(_req: Request<()>) -> Result<Response, tide::Error> {
     Ok(res)
 }
 
+#[derive(Debug, Clone)]
+pub struct SitemapProvider {
+    sitemap: docs::Sitemap,
+}
+
+#[tide::utils::async_trait]
+impl<T: Clone + Send + Sync + 'static> tide::Middleware<T> for SitemapProvider {
+    async fn handle(&self, mut req: Request<T>, next: tide::Next<'_, T>) -> tide::Result {
+        req.set_ext(self.sitemap.clone());
+        Ok(next.run(req).await)
+    }
+}
+
 #[async_std::main]
 async fn main() -> Result<(), std::io::Error> {
+    let sitemap = docs::construct_sitemap()?;
+    println!("Initialized doc sitemap with {} entries", &sitemap.num_entries);
     let mut app = tide::new();
     app.at("/").get(index_handler);
     app.at("/docs").get(docs::docs_handler);
     app.at("/docs/:path").get(docs::docs_handler);
     app.at("/static").serve_dir("static/")?;
     app.with(driftwood::DevLogger);
+    app.with(SitemapProvider { sitemap });
     println!("rbb is starting at http://localhost:8080");
     app.listen("127.0.0.1:8080").await?;
     Ok(())
